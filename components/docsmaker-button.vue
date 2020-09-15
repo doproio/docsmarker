5a80bf7eec04a254ce42868f103edf99d5d454d6
jeakey <june_fh@qq.com>
2020-09-15 10:58:37 +0800

    feat：增加example示例能力，可以自主定义示例代码，去掉按照props罗列的能力

--- components/docsmaker-button.vue
+++ components/docsmaker-button.vue
@@ -1,15 +1,24 @@
 <template>
-    <div class="ui-btn">{{text}}</div>
+    <div :class="['ui-btn','ui-btn-'+type]">{{text}}</div>
 </template>
 <script>
+// @example {"type":"text","text":"按钮"}
+// @example {"type":"defalut","text":"确定"}
+// 带@example前缀的为示例代码，必须为json格式（比如{"type":"text","text":"按钮"}），会罗列所有example的props键值对到demo示意里面，如果没有example，则会从将所有props的默认值（第一个）中提出来展示
 export default {
-    props:{
-        text:{
-            // ["按钮","确定"]
-            type:String,
-            default:'按钮'
-        }
-    }
+    props: {
+        
+        text: {
+            // "按钮" / "确定"
+            type: String,
+            default: '按钮',
+        },
+        type: {
+            // "text" / "default" 
+            type: String,
+            default: 'text',
+        },
+    },
 }
 </script>
 
@@ -27,5 +36,8 @@ export default {
     &:active{
         opacity: .5;
     }
+}
+.ui-btn-text{
+    border-color:rgba(0,0,0,0)
 }
 </style>
